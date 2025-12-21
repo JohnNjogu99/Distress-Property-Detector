1a9383b4b725d502f7010b364982add9e8ed3a99
JohnNjogu <johnnjogu99@gmail.com>
2025-12-21 14:52:20 +0300

    Add FavoriteListView and FavoriteDetailView for user favorites and PropertyListView and PropertyDetailView for Property API,NotificationListView and NotificationDetailView for Notification API

--- backend/config/notifications/views.py
+++ backend/config/notifications/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Notification
+from .serializers import NotificationSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+class NotificationListView(generics.ListCreateAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
+
+class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user)
